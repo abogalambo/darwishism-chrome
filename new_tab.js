b975dea2972e84a737303bc71767177897b5bbf9
Yaser Alashry <yaser.alashry@xing.com>
2018-01-17 11:47:48 +0100

    use https instead of http

--- new_tab.js
+++ new_tab.js
@@ -1,15 +1,14 @@
 var xhr = new XMLHttpRequest();
-xhr.open('GET', 'http://api.tumblr.com/v2/blog/darwishism.tumblr.com/posts/photo?api_key=jsjlkvG5UxM1yFZoB1hzmgKsm5hAzuimqJGstkxlknvYyq3gfE');
+var offset = Math.floor(Math.random() * 100)
+xhr.open('GET', 'https://api.tumblr.com/v2/blog/darwishism.tumblr.com/posts/photo?api_key=jsjlkvG5UxM1yFZoB1hzmgKsm5hAzuimqJGstkxlknvYyq3gfE&offset=' + offset);
 xhr.setRequestHeader('Content-Type', 'application/json');
 xhr.onload = function() {
-	if (xhr.status === 200) {
-		var images = JSON.parse(xhr.responseText).response.posts.map(function(post){
-			return post.photos[0].original_size.url
-		});
-
-		var imageIndex = Math.floor(Math.random() * images.length)
-		document.getElementById("darwishism-post").src = images[imageIndex];
-		console.log(images)
-	}
+  if (xhr.status === 200) {
+    var images = JSON.parse(xhr.responseText).response.posts.map(function(post){
+      return post.photos[0].original_size.url
+    });
+    var imageIndex = Math.floor(Math.random() * images.length)
+    document.getElementById("darwishism-post").src = images[imageIndex];
+  }
 };
 xhr.send();
