c41997a0deca0705d50ee236390ec93f5f2084d2
abogalambo <yaser.al3shry@gmail.com>
2018-01-21 18:43:42 +0100

    load only one image post

--- new_tab.js
+++ new_tab.js
@@ -1,14 +1,13 @@
 var xhr = new XMLHttpRequest();
-var offset = Math.floor(Math.random() * 100)
-xhr.open('GET', 'https://api.tumblr.com/v2/blog/darwishism.tumblr.com/posts/photo?api_key=jsjlkvG5UxM1yFZoB1hzmgKsm5hAzuimqJGstkxlknvYyq3gfE&offset=' + offset);
+var offset = Math.floor(Math.random() * 200)
+xhr.open('GET', 'https://api.tumblr.com/v2/blog/darwishism.tumblr.com/posts/photo?api_key=jsjlkvG5UxM1yFZoB1hzmgKsm5hAzuimqJGstkxlknvYyq3gfE&limit=1&offset=' + offset);
 xhr.setRequestHeader('Content-Type', 'application/json');
 xhr.onload = function() {
   if (xhr.status === 200) {
-    var posts = JSON.parse(xhr.responseText).response.posts
-    var postIndex = Math.floor(Math.random() * posts.length)
-    var post = posts[postIndex]
+    var post = JSON.parse(xhr.responseText).response.posts[0]
     document.getElementById("darwishism-post").src = post.photos[0].original_size.url;
     document.getElementById("translation-box").innerHTML = post.summary;
+    document.getElementById("avatar").style = {display: ''};
   }
 };
 xhr.send();
