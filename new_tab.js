91ffe04326fd87d8b579ad97c3f9fd8a7850e8dd
abogalambo <yaser.al3shry@gmail.com>
2018-01-28 21:22:18 +0300

    increase photos limit

--- new_tab.js
+++ new_tab.js
@@ -1,5 +1,5 @@
 var xhr = new XMLHttpRequest();
-var offset = Math.floor(Math.random() * 200)
+var offset = Math.floor(Math.random() * 285)
 xhr.open('GET', 'https://api.tumblr.com/v2/blog/darwishism.tumblr.com/posts/photo?api_key=jsjlkvG5UxM1yFZoB1hzmgKsm5hAzuimqJGstkxlknvYyq3gfE&limit=1&offset=' + offset);
 xhr.setRequestHeader('Content-Type', 'application/json');
 xhr.onload = function() {
