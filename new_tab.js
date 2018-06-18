a36206e9217c764a05d701c09e768a5f724026c7
abogalambo <yaser.al3shry@gmail.com>
2018-06-18 02:01:43 +0200

    use post URL instead of blog URL

--- new_tab.js
+++ new_tab.js
@@ -3,6 +3,7 @@ var offset = Math.floor(Math.random() * 285)
 xhr.open('GET', 'https://api.tumblr.com/v2/blog/darwishism.tumblr.com/posts/photo?api_key=jsjlkvG5UxM1yFZoB1hzmgKsm5hAzuimqJGstkxlknvYyq3gfE&limit=1&offset=' + offset);
 xhr.setRequestHeader('Content-Type', 'application/json');
 
+
 xhr.onload = function() {
   if (xhr.status === 200) {
     var post = JSON.parse(xhr.responseText).response.posts[0]
@@ -10,6 +11,7 @@ xhr.onload = function() {
     var photoIndex = Math.floor(Math.random() * photos.length)
     document.getElementById("darwishism-post").src = photos[photoIndex].original_size.url;
     document.getElementById("avatar").style = {display: ''};
+    document.getElementById("avatar").href = post.post_url;
 
     var year2018 = (new Date('1/1/2018')).getTime() / 1000;
     var recentPost = post.timestamp > year2018;
